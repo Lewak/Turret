24fc6c37eb33bec6e5cae7f706ca2ee41d0bbe11
Bober RS <boberrs@gmail.com>
2022-12-10 13:16:20 +0100

    Project cleanup

--- Logic/peripherals.py
+++ Logic/peripherals.py
@@ -20,7 +20,7 @@ class Peripherals:
         Peripherals.laser = Laser(4)
         Peripherals.laser.turnOff()
         Peripherals.gimbal = Gimbal(3, 2)
-        # used audio pins (PCM): 19 (LRC); 18 (BCLK); 21 (DIN)
+        # used audio pins (PCM): GPIO19 (LRC); GPIO18 (BCLK); GPIO21 (DIN)
         # https://learn.adafruit.com/adafruit-max98357-i2s-class-d-mono-amp/raspberry-pi-wiring
         Peripherals.audio = AudioInterface()
         Peripherals.button = Button(15)
