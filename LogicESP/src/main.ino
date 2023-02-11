be0223e9b7aa4e8462a9d9fc6cf035a63d284a2c
Bober RS <boberrs@gmail.com>
2023-02-11 18:17:56 +0100

    working initial setup for esp + drv8825 + nema 17

--- LogicESP/src/main.ino
+++ LogicESP/src/main.ino
@@ -1,24 +1,29 @@
-/*********
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/vs-code-platformio-ide-esp32-esp8266-arduino/
-*********/
-
 #include <Arduino.h>
 
 #define LED 2
+#define STEP_X 32 
+#define STEP_Y 33 
+#define DIR_X 25 
+#define DIR_Y 26 
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(LED, OUTPUT);
+  pinMode(STEP_X, OUTPUT);
+  pinMode(STEP_Y, OUTPUT);
+  pinMode(DIR_X, OUTPUT);
+  pinMode(DIR_Y, OUTPUT);
+  digitalWrite(DIR_X, HIGH);
+  digitalWrite(DIR_Y, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED, HIGH);
-  Serial.println("LED is on");
-  delay(5000);
-  digitalWrite(LED, LOW);
-  Serial.println("LED is off");
-  delay(5000);
+  digitalWrite(STEP_X, HIGH);
+  digitalWrite(STEP_Y, HIGH);
+  Serial.println("step on");
+  delay(1);
+  digitalWrite(STEP_X, LOW);
+  digitalWrite(STEP_Y, LOW);
+  Serial.println("step off");
+  delay(1);
 }
